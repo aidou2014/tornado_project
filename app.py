86565771edb18894e3938586e2babe80defacd20
aidou2014 <1285153026@qq.com>
2019-02-25 20:56:29 +0800

    项目初始化，基本的三个页面

---
+++ app.py
@@ -0,0 +1,34 @@
+import tornado.ioloop
+import tornado.web
+import tornado.options
+from tornado.options import define, options
+
+# from handlers.main import IndexHandler, ExploreHandler, PostHandler
+from handlers import main
+
+define('port', default=8080, help='listen port', type=int)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", main.IndexHandler),
+            (r"/explore", main.ExploreHandler),
+            (r"/post/(?P<post_id>[0-9]+)", main.PostHandler),
+        ]
+        settings = dict(
+            debug=True,
+            template_path = 'templates',
+            static_path = 'static',
+            # static_url_prefix = '/pics/'
+        )
+        super().__init__(handlers, **settings)
+
+
+application = Application()
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    application.listen(options.port)
+    print('Server start on port {}'.format(options.port))
+    tornado.ioloop.IOLoop.current().start()
---
+++ handlers/main.py
@@ -0,0 +1,19 @@
+import tornado.web
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    """首页，说管子的用户图片流"""
+    def get(self):
+        self.render('index.html')
+
+
+class ExploreHandler(tornado.web.RequestHandler):
+    """发现货最近上传的图片页面"""
+    def get(self):
+        self.render('explore.html')
+
+
+class PostHandler(tornado.web.RequestHandler):
+    """单个图片详情页面"""
+    def get(self,post_id):
+        self.render('post.html',post_id=post_id)
