ed3f5269cacaf190d7ac90c8089308cf4ca788d4
aidou2014 <1285153026@qq.com>
2019-02-26 16:02:14 +0800

    版本迁移以及数据库管理alembic软件

--- models/account.py
+++ models/account.py
@@ -4,7 +4,7 @@ from .db import Base
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(100), unique=True, nullable=False)
     password = Column(String(200), nullable=False)
